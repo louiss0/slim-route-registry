c8c3c3e693eb6b777e0da96b4a8f710859308fce
Shelton <louisshelton0@gmail.com>
2021-10-06 12:22:43 -0400

    changed entire added test middleware and controllers

--- tests/Test2ControllerTest.php
+++ tests/Test2ControllerTest.php
@@ -1,14 +1,98 @@
 <?php
 
-
-
+use DI\Bridge\Slim\Bridge;
+use Louiss0\SlimRouteRegistry\Controllers\Test2Controller;
+use Louiss0\SlimRouteRegistry\RouteRegistry;
 use PHPUnit\Framework\TestCase;
+use Slim\Interfaces\RouteInterface;
 
 class Test2ControllerTest extends TestCase
 {
 
+
+    public function getApp()
+    {
+        # code...
+        return Bridge::create();
+    }
+
+
+    public function setupRouteRegistry()
+    {
+        # code...
+
+        RouteRegistry::setup($this->getApp());
+    }
+
+
     public function testAutomaticRouteMethodsAreRegisteredProperly()
     {
         # code...
+
+
+        $this->setupRouteRegistry();
+
+        RouteRegistry::resource("", Test2Controller::class);
+
+
+        $route_info = RouteRegistry::getRoutes();
+
+        $changed_info = array_map(
+            function (RouteInterface $value) {
+                # code...
+
+                return [$value->getMethods(), $value->getCallable()];
+            },
+            $route_info
+        );
+
+        $expected = [
+            "route0" => [["GET"], [Test2Controller::class, Test2Controller::GET_ALL]],
+            "route1" => [["GET"], [Test2Controller::class, Test2Controller::GET_ONE]],
+            "route2" => [["POST"], [Test2Controller::class, Test2Controller::CREATE]],
+            "route3" => [["PUT"], [Test2Controller::class, Test2Controller::UPDATE_OR_CREATE_ONE]],
+            "route4" => [["PATCH"], [Test2Controller::class, Test2Controller::UPDATE_ONE]],
+            "route5" => [["DELETE"], [Test2Controller::class, Test2Controller::DESTROY_ONE]],
+        ];
+
+        $this->assertSame($expected, $changed_info);
+    }
+
+
+    public function testAutomaticRouteMethodsAreRegisteredProperlyUnderAGroup()
+    {
+        # code...
+
+
+        $this->setupRouteRegistry();
+
+
+        RouteRegistry::group("/api", function () {
+
+            RouteRegistry::resource("/test", Test2Controller::class);
+        });
+
+
+        $route_info = RouteRegistry::getRoutes();
+
+        $changed_info = array_map(
+            function (RouteInterface $value) {
+                # code...
+
+                return [$value->getPattern(), $value->getCallable()];
+            },
+            $route_info
+        );
+
+        $expected = [
+            "route0" => ["/api/test", [Test2Controller::class, Test2Controller::GET_ALL]],
+            "route1" => ["/api/test/{id:\d+}", [Test2Controller::class, Test2Controller::GET_ONE]],
+            "route2" => ["/api/test", [Test2Controller::class, Test2Controller::CREATE]],
+            "route3" => ["/api/test", [Test2Controller::class, Test2Controller::UPDATE_OR_CREATE_ONE]],
+            "route4" => ["/api/test/{id:\d+}", [Test2Controller::class, Test2Controller::UPDATE_ONE]],
+            "route5" => ["/api/test/{id:\d+}", [Test2Controller::class, Test2Controller::DESTROY_ONE]],
+        ];
+
+        $this->assertSame($expected, $changed_info);
     }
 }
